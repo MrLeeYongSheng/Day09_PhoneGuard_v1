37539f38d918c7f17f4eed9f66af909cf981c0ad
MrLeeYongSheng <731153234@qq.com>
2017-04-20 21:15:07 +0800

    一键锁屏并且设置密码
    利用设备管理器来执行操作,使用了locknow方法来锁屏,resetpassword方法来设置密码,详细配置方法在API文档有

---
+++ src/com/itheima/day09_phoneguard_v1/receiver/DeviceAdminSample.java
@@ -0,0 +1,7 @@
+package com.itheima.day09_phoneguard_v1.receiver;
+
+import android.app.admin.DeviceAdminReceiver;
+
+public class DeviceAdminSample extends DeviceAdminReceiver {
+
+}
--- src/com/itheima/day09_phoneguard_v1/service/LostFindService.java
+++ src/com/itheima/day09_phoneguard_v1/service/LostFindService.java
@@ -1,6 +1,7 @@
 package com.itheima.day09_phoneguard_v1.service;
 
 import android.app.Service;
+import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -40,6 +41,12 @@ public class LostFindService extends Service {
 				if("#*gps*#".equals(sm.getMessageBody())) {
 					Intent locationIntent = new Intent(context, LocationService.class);
 					startService(locationIntent);
+					abortBroadcast();
+				} else if("#*lockscreen*#".equals(sm.getMessageBody())) {
+					DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+					dpm.lockNow();
+					dpm.resetPassword("123456", 0);
+					abortBroadcast();
 				}
 			}
 		}
