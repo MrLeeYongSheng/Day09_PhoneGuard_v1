5dbd684bc4f71278871b2edf46ba216e2e5569c0
MrLeeYongSheng <731153234@qq.com>
2017-04-20 23:52:07 +0800

    清除SD卡,播放警报音乐
    清除SD卡:利用设备管理器来清除SD卡的数据,清除数据的时候手机会重启
    播放警报音乐:使用Mediaplayer来播放音乐,做了只播放一次的处理

--- src/com/itheima/day09_phoneguard_v1/service/LostFindService.java
+++ src/com/itheima/day09_phoneguard_v1/service/LostFindService.java
@@ -6,13 +6,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.telephony.SmsMessage;
 
+import com.itheima.day09_phoneguard_v1.R;
+
 public class LostFindService extends Service {
 
 	private SmsReveiver receiver;
+	private boolean isPlay = false;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -38,15 +43,37 @@ public class LostFindService extends Service {
 			Object[] objects = (Object[]) extras.get("pdus");
 			for (Object object : objects) {
 				SmsMessage sm = SmsMessage.createFromPdu((byte[]) object);
-				if("#*gps*#".equals(sm.getMessageBody())) {
+				String sms = sm.getMessageBody();
+				if("#*gps*#".equals(sms)) {
 					Intent locationIntent = new Intent(context, LocationService.class);
 					startService(locationIntent);
 					abortBroadcast();
-				} else if("#*lockscreen*#".equals(sm.getMessageBody())) {
+				} else if("#*lockscreen*#".equals(sms)) {
 					DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
 					dpm.lockNow();
 					dpm.resetPassword("123456", 0);
 					abortBroadcast();
+				} else if("#*wipedata*#".equals(sms)){
+					DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+					dpm.wipeData(DevicePolicyManager.WIPE_EXTERNAL_STORAGE);
+					abortBroadcast();
+				} else if("#*music*#".equals(sms)) {
+					abortBroadcast();
+					if(isPlay) {
+						return ;
+					}
+					MediaPlayer mp = MediaPlayer.create(LostFindService.this, R.raw.music1);
+					mp.setVolume(1f, 1f);
+					mp.start();
+					isPlay = true;
+					mp.setOnCompletionListener(new OnCompletionListener() {
+						
+						@Override
+						public void onCompletion(MediaPlayer mp) {
+
+							isPlay = false;
+						}
+					});
 				}
 			}
 		}
